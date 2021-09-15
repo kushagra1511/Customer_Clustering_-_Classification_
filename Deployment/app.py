5b137f8a5e1afc1338ea6f672486e219a8f4fda7
Kushagra Kapoor <80125585+kushagra1511@users.noreply.github.com>
2021-09-15 10:12:35 +0530

    Add files via upload

---
+++ Deployment/app.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pickle
+
+
+
+knn=pickle.load(open('knn.pkl','rb'))
+tree=pickle.load(open('tree.pkl','rb'))
+rf=pickle.load(open('rf.pkl','rb'))
+
+def classify(n):
+    if n == 'Class 1':
+        return 'Class 1 (Young middle responsible behaviour)'
+    elif n == 'Class 2':
+        return 'Class 2 (Old middle responsible behaviour)'
+    elif n == 'Class 3':
+        return 'Class 3 (Aged rich miser behaviour)'
+    elif n == 'Class 4':
+        return 'Class 4 (Young rich spendthrift behaviour)'
+    elif n == 'Class 5':
+        return 'Class 5 (Young poor spendthrift behaviour)'
+    elif n == 'Class 6':
+        return 'Class 6 (Aged poor responsible behaviour)'
+    else:
+        return 'Unidentified Customer Class'
+    
+def main():
+    st.title("Customer Classifier ML App")
+    html_temp = """
+    <div style="background-color:teal ;padding:10px">
+    <h2 style="color:white;text-align:center;">Customer Classification</h2>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+    activities=['K - Nearest Neighbour (Accuracy = 97.5%)','Decision Tree (Accuracy = 92.5%)', 'Random Forest (Accuracy = 95.0%)']
+    option=st.sidebar.selectbox('Which model would you like to use?',activities)
+    st.subheader(option)
+    
+    sex=["Male", "Female"]
+    gender=sex.index(st.selectbox('Select Gender', sex))
+    age=st.slider('Select Age', 18.0, 70.0)
+    a_income=st.slider('Select Annual Income (k$)', 0.0, 150.0)
+    s_score=st.slider('Select Spending Score', 1.0, 100.0)
+    
+    inputs=[[age,a_income,s_score,gender]]
+    
+    if st.button('Classify'):
+        if option=='K - Nearest Neighbour (Accuracy = 97.5%)':
+            st.success(classify(knn.predict(inputs)))
+        elif option=='Decision Tree (Accuracy = 92.5%)':
+            st.success(classify(tree.predict(inputs)))
+        elif option=='Random Forest (Accuracy = 95.0%)':
+            st.success(classify(rf.predict(inputs)))
+            
+
+if __name__=='__main__':
+    main()
